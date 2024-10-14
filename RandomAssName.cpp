8509de16acfe2bb261aae9e478619f03aed2d879
Rhoy Jayson L Suyat <endlesrim@gmail.com>
2024-10-14 10:21:21 +0800

    Add files via upload

---
+++ RandomAssName.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	
+	int sugarPriceUSD = 0;
+	int ricePricePound = 0;
+	int sardinesPricePound = 0;
+	int coffeePriceUSD = 0;
+	int milkPriceUSD = 0;
+	
+	float USD = 50, Pound = 2, Php = 1,
+	float TotalCostPHP;
+	
+	int sugarQty; 
+	int riceQty;
+	int sardinesQty; 
+	int coffeeQty; 
+	int milkQty;
+	
+	float usdToPhp;
+	float pountToPhp;
+	
+	cout << "Sugaer" << endl;
+	cout << "Rice" << endl;
+	cout << "Sardines" << endl;
+	cout << "Coffee" << endl;
+	cout << "Milk" << endl;
+	
+	
+	cout << "-----------------------------------------------" << endl;
+	
+	cout << "How many sugar: ";
+	cin >> sugarQty;
+	
+	cout << "How many rice: ";
+	cin >> riceQty;
+	
+	cout << "How many sardines: ";
+	cin >> sardinesQty;
+	
+	cout << "How many coffee: ";
+	cin >> coffeeQty;
+	
+	cout << "How many milk: ";
+	cin >> milkQty;
+	
+	
+	
+	cout << "";
+	
+	
+	
+	return 0;
+	
+}
